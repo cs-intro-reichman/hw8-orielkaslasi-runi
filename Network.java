7b71ccac7a93206400cc458f20eae55d8af82103
Ori Elkaslasi <ori.elkaslasi@post.runi.ac.il>
2026-01-09 13:57:32 +0200

    test 6

--- Network.java
+++ Network.java
@@ -58,15 +58,22 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        if (name1.equals(name2)) return false;
-        User u1 = getUser(name1);
-        User u2 = getUser(name2);
+    if (name1 == null || name2 == null) {
+    return false;
+    }
+    if (name1.equals(name2)) {
+        return false;
+    }
 
-        if (u1 == null || u2 == null) {
-          return false;
-        }
-        return u1.addFollowee(name2);
-        }
+    User u1 = getUser(name1);
+    User u2 = getUser(name2);
+
+    if (u1 == null || u2 == null) {
+        return false;
+    }
+
+    return u1.addFollowee(name2);
+}
       
     /** For the user with the given name, recommends another user to follow. The recommended user is
      *  the user that has the maximal mutual number of followees as the user with the given name. */
